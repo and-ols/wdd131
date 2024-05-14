30236f2e80ba081d0ca486b5d1ef9ad11f1fa535
Andrew Olson <ols21007@byui.edu>
2024-05-14 15:01:18 -0600

    Activity one

---
+++ ponder/ponderW04/debugging.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+
+area = radius * radius * PI;
+console.log(area);
+
+radius = 4;
+console.log(radius);
+
+area = radius * radius * PI;
+
+console.log(area);
