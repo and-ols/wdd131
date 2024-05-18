c8060bd407608e39d323c08ffbe2bc8da7d73832
Andrew Olson <ols21007@byui.edu>
2024-05-18 16:40:53 -0600

    Views image and closes it

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -25,22 +25,10 @@ window.addEventListener('resize', handleResize);
 function viewerTemplate(picture, altText) {
    return `
     <div class="viewer">
-    <button class="close-viewer">X</button>
+    <button class="close-viewer">❌</button>
     <img src="${picture}" alt="${altText}">
     </div>`
 }
-
-
-const viewer = document.querySelector('.viewer');
-
-function closeImage () {
-   const closeViewerBtn = document.querySelector('.close-viewer');
-
-   closeViewerBtn.addEventListener('click', () => {
-      viewer.classList.toggle('close-pic'); 
-   });
-}
-   
 
 function viewHandler(event) {
 	// create a variable to hold the element that was clicked on from event.target
@@ -48,21 +36,28 @@ function viewHandler(event) {
 	// get the src attribute from that element and 'split' it on the "-"
    const imgLink = target.src;
    const split = imgLink.split('-');
-
    const imgName = split[0];
 
-
-
 	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
-   const fullImage = '${norris}-full.jpeg';
+   const fullImage = 'norris-full.jpeg';
 
 	// insert the viewerTemplate into the top of the body element
    const body = document.body;
 	body.insertAdjacentHTML("afterbegin", viewerTemplate(fullImage, imgName));
 
 	// add a listener to the close button (X) that calls a function called closeViewer when clicked
-   closeImage();
+
+   const closeViewerBtn = document.querySelector('.close-viewer');
+
+   function closeViewer() {
+      const viewer = document.querySelector('.viewer');
+      viewer.remove();
+   }
+
+   closeViewerBtn.addEventListener('click', () => {
+         closeViewer();
+   });
 }
 
-viewHandler();
-
+const images = document.querySelectorAll('.gallery img');
+images.forEach(image => image.addEventListener('click', viewHandler));
