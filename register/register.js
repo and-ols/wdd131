36e079e4695207857ee54f194d770bb31d4104c0
Andrew Olson <ols21007@byui.edu>
2024-06-12 18:37:53 -0600

    Worked on JS

---
+++ register/register.js
@@ -0,0 +1,11 @@
+
+
+const addParticipantBtn = document.getElementById("add");
+
+addParticipantBtn.addEventListener("click", )
+
+
+// Count is the current number of participants shown
+function participantTemplate(count) {
+   let participant = 1;
+}
